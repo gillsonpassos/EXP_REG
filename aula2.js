1fb5f09dc910e1733d003763dc457dc287822589
gillsonpassos <oliveirapassos07@outlook.com>
2022-10-18 19:46:37 -0300

    String Match e Replace

---
+++ aula2.js
@@ -0,0 +1,13 @@
+const { texto } = require("./base");
+const regExp1 = /joão|Maria/gi;
+
+// (...())(...)(...) $1 $2 $3
+
+//console.log(texto);
+//console.log(texto.match(regExp1));
+//console.log(texto.replace(/(joão|Maria)/gi, "<b>$1</b>"));
+console.log(
+  texto.replace(/(joão|Maria)/gi, function (input) {
+    return " ########### " + input.toUpperCase() + " ########### ";
+  })
+);
