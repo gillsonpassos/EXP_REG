3448e4c59c71e4b3a86df363f12a11ea5a72d555
gillsonpassos <oliveirapassos07@outlook.com>
2022-10-18 19:50:19 -0300

    Replace

--- aula2.js
+++ aula2.js
@@ -1,11 +1,11 @@
 const { texto } = require("./base");
 const regExp1 = /joão|Maria/gi;
 
-// (...())(...)(...) $1 $2 $3
+//(...())(...)(...) $1 $2 $3
 
-//console.log(texto);
-//console.log(texto.match(regExp1));
-//console.log(texto.replace(/(joão|Maria)/gi, "<b>$1</b>"));
+console.log(texto);
+console.log(texto.match(regExp1));
+console.log(texto.replace(/(joão|Maria)/gi, "<b>$1</b>"));
 console.log(
   texto.replace(/(joão|Maria)/gi, function (input) {
     return " ########### " + input.toUpperCase() + " ########### ";
