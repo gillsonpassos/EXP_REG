fceaec2b6916deb9e9546c095312e82d5e562bce
gillsonpassos <oliveirapassos07@outlook.com>
2022-10-21 12:01:47 -0300

    Exercicios com cpf e ips

--- base.js
+++ base.js
@@ -21,6 +21,27 @@ const arquivos = [
   "lista de compras.txt",
 ];
 
+const cpfs = `Os CPFs são:
+254.224.877-45  215.978.456-12  047.258.369-96 
+ 
+ 
+ 
+ 963.987.321-00
+ `;
+
+const ips = `
+ 
+ Os Ips são:
+    0.0.0.0
+    
+    192.168.0.25
+
+         10.10.5.12
+
+    255.255.255.255
+ 
+ `;
+
 //             <*>    *   </*>  <*>     *     </*>
 //             <.+>,+ <\/.+>
 const alfabeto =
@@ -32,4 +53,6 @@ module.exports = {
   arquivos,
   html,
   alfabeto,
+  cpfs,
+  ips,
 };
